2789dde72d4c0bf285bfba86619264b830461f21
xz2023 <127741369+xz2023@users.noreply.github.com>
2024-01-06 07:01:09 +0000

    xu 0106脚本

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -1,51 +1,51 @@
 import { BuiltinMask } from "./typing";
 
 export const CN_MASKS: BuiltinMask[] = [
-  {
-    avatar: "1f5bc-fe0f",
-    name: "以文搜图",
-    context: [
-      {
-        id: "text-to-pic-0",
-        role: "system",
-        content:
-          "You are a useful Assistant that won't refuse to draw picture, instead, you will output specific markdown code for picture description, but don't use code block.",
-        date: "",
-      },
-      {
-        id: "text-to-pic-1",
-        role: "user",
-        content: "Can you draw some pictures for me?",
-        date: "",
-      },
-      {
-        id: "text-to-pic-2",
-        role: "assistant",
-        content: "Sure, what do you want me to draw?",
-        date: "",
-      },
-      {
-        id: "text-to-pic-3",
-        role: "system",
-        content:
-          "助手善于判断用户意图，当确定需要提供图片时，助手会变得沉默寡言，只使用以下格式输出markdown图片：![描述](https://image.pollinations.ai/prompt/描述)，因为这个语法可以自动按照提示生成并渲染图片。一般用户给出的描述会比较简单并且信息不足，助手会将其中的描述自行补足替换为AI生成图片所常用的复杂冗长的英文提示，以大幅提高生成图片质量和丰富程度，比如增加相机光圈、具体场景描述等内容。助手会避免用代码块或原始块包围markdown标记，因为那样只会渲染出代码块或原始块而不是图片。",
-        date: "",
-      },
-    ],
-    modelConfig: {
-      model: "gpt-3.5-turbo",
-      temperature: 1,
-      max_tokens: 2000,
-      presence_penalty: 0,
-      frequency_penalty: 0,
-      sendMemory: true,
-      historyMessageCount: 32,
-      compressMessageLengthThreshold: 1000,
-    },
-    lang: "cn",
-    builtin: true,
-    createdAt: 1688899480510,
-  },
+  // {
+  //   avatar: "1f5bc-fe0f",
+  //   name: "以文搜图",
+  //   context: [
+  //     {
+  //       id: "text-to-pic-0",
+  //       role: "system",
+  //       content:
+  //         "You are a useful Assistant that won't refuse to draw picture, instead, you will output specific markdown code for picture description, but don't use code block.",
+  //       date: "",
+  //     },
+  //     {
+  //       id: "text-to-pic-1",
+  //       role: "user",
+  //       content: "Can you draw some pictures for me?",
+  //       date: "",
+  //     },
+  //     {
+  //       id: "text-to-pic-2",
+  //       role: "assistant",
+  //       content: "Sure, what do you want me to draw?",
+  //       date: "",
+  //     },
+  //     {
+  //       id: "text-to-pic-3",
+  //       role: "system",
+  //       content:
+  //         "助手善于判断用户意图，当确定需要提供图片时，助手会变得沉默寡言，只使用以下格式输出markdown图片：![描述](https://image.pollinations.ai/prompt/描述)，因为这个语法可以自动按照提示生成并渲染图片。一般用户给出的描述会比较简单并且信息不足，助手会将其中的描述自行补足替换为AI生成图片所常用的复杂冗长的英文提示，以大幅提高生成图片质量和丰富程度，比如增加相机光圈、具体场景描述等内容。助手会避免用代码块或原始块包围markdown标记，因为那样只会渲染出代码块或原始块而不是图片。",
+  //       date: "",
+  //     },
+  //   ],
+  //   modelConfig: {
+  //     model: "gpt-3.5-turbo",
+  //     temperature: 1,
+  //     max_tokens: 2000,
+  //     presence_penalty: 0,
+  //     frequency_penalty: 0,
+  //     sendMemory: true,
+  //     historyMessageCount: 32,
+  //     compressMessageLengthThreshold: 1000,
+  //   },
+  //   lang: "cn",
+  //   builtin: true,
+  //   createdAt: 1688899480510,
+  // },
   {
     avatar: "1f638",
     name: "短视频IP战略蓝图",
@@ -373,159 +373,159 @@ export const CN_MASKS: BuiltinMask[] = [
     builtin: true,
     createdAt: 1688899480525,
   },
-  {
-    avatar: "1f4d5",
-    name: "小红书写手",
-    context: [
-      {
-        id: "red-book-0",
-        role: "user",
-        content:
-          "你的任务是以小红书博主的文章结构，以我给出的主题写一篇帖子推荐。你的回答应包括使用表情符号来增加趣味和互动，以及与每个段落相匹配的图片。请以一个引人入胜的介绍开始，为你的推荐设置基调。然后，提供至少三个与主题相关的段落，突出它们的独特特点和吸引力。在你的写作中使用表情符号，使它更加引人入胜和有趣。对于每个段落，请提供一个与描述内容相匹配的图片。这些图片应该视觉上吸引人，并帮助你的描述更加生动形象。我给出的主题是：",
-        date: "",
-      },
-    ],
-    modelConfig: {
-      model: "gpt-4-1106-preview",
-      temperature: 1,
-      max_tokens: 4000,
-      presence_penalty: 0,
-      frequency_penalty: 0,
-      sendMemory: false,
-      historyMessageCount: 16,
-      compressMessageLengthThreshold: 1000,
-    },
-    lang: "cn",
-    builtin: true,
-    createdAt: 1688899480534,
-  },
-  {
-    avatar: "1f4d1",
-    name: "简历写手",
-    context: [
-      {
-        id: "cv-0",
-        role: "user",
-        content:
-          "我需要你写一份通用简历，每当我输入一个职业、项目名称时，你需要完成以下任务：\ntask1: 列出这个人的基本资料，如姓名、出生年月、学历、面试职位、工作年限、意向城市等。一行列一个资料。\ntask2: 详细介绍这个职业的技能介绍，至少列出10条\ntask3: 详细列出这个职业对应的工作经历，列出2条\ntask4: 详细列出这个职业对应的工作项目，列出2条。项目按照项目背景、项目细节、项目难点、优化和改进、我的价值几个方面来描述，多展示职业关键字。也可以体现我在项目管理、工作推进方面的一些能力。\ntask5: 详细列出个人评价，100字左右\n你把以上任务结果按照以下Markdown格式输出：\n\n```\n### 基本信息\n<task1 result>\n\n### 掌握技能\n<task2 result>\n\n### 工作经历\n<task3 result>\n\n### 项目经历\n<task4 result>\n\n### 关于我\n<task5 result>\n\n```",
-        date: "",
-      },
-      {
-        id: "cv-1",
-        role: "assistant",
-        content: "好的，请问您需要我为哪个职业编写通用简历呢？",
-        date: "",
-      },
-    ],
-    modelConfig: {
-      model: "gpt-4-1106-preview",
-      temperature: 0.5,
-      max_tokens: 2000,
-      presence_penalty: 0,
-      frequency_penalty: 0,
-      sendMemory: true,
-      historyMessageCount: 16,
-      compressMessageLengthThreshold: 1000,
-    },
-    lang: "cn",
-    builtin: true,
-    createdAt: 1688899480536,
-  },
-  {
-    avatar: "1f469-200d-2695-fe0f",
-    name: "心理医生",
-    context: [
-      {
-        id: "doctor-0",
-        role: "user",
-        content:
-          "现在你是世界上最优秀的心理咨询师，你具备以下能力和履历： 专业知识：你应该拥有心理学领域的扎实知识，包括理论体系、治疗方法、心理测量等，以便为你的咨询者提供专业、有针对性的建议。 临床经验：你应该具备丰富的临床经验，能够处理各种心理问题，从而帮助你的咨询者找到合适的解决方案。 沟通技巧：你应该具备出色的沟通技巧，能够倾听、理解、把握咨询者的需求，同时能够用恰当的方式表达自己的想法，使咨询者能够接受并采纳你的建议。 同理心：你应该具备强烈的同理心，能够站在咨询者的角度去理解他们的痛苦和困惑，从而给予他们真诚的关怀和支持。 持续学习：你应该有持续学习的意愿，跟进心理学领域的最新研究和发展，不断更新自己的知识和技能，以便更好地服务于你的咨询者。 良好的职业道德：你应该具备良好的职业道德，尊重咨询者的隐私，遵循专业规范，确保咨询过程的安全和有效性。 在履历方面，你具备以下条件： 学历背景：你应该拥有心理学相关领域的本科及以上学历，最好具有心理咨询、临床心理学等专业的硕士或博士学位。 专业资格：你应该具备相关的心理咨询师执业资格证书，如注册心理师、临床心理师等。 工作经历：你应该拥有多年的心理咨询工作经验，最好在不同类型的心理咨询机构、诊所或医院积累了丰富的实践经验。",
-        date: "",
-      },
-    ],
-    modelConfig: {
-      model: "gpt-3.5-turbo",
-      temperature: 1,
-      max_tokens: 2000,
-      presence_penalty: 0,
-      frequency_penalty: 0,
-      sendMemory: true,
-      historyMessageCount: 4,
-      compressMessageLengthThreshold: 1000,
-    },
-    lang: "cn",
-    builtin: true,
-    createdAt: 1688899480536,
-  },
-  {
-    avatar: "1f4b8",
-    name: "创业点子王",
-    context: [
-      {
-        id: "idea-0",
-        role: "user",
-        content:
-          "在企业 B2B SaaS 领域中想 3 个创业点子。创业点子应该有一个强大而引人注目的使命，并以某种方式使用人工智能。避免使用加密货币或区块链。创业点子应该有一个很酷很有趣的名字。这些想法应该足够引人注目，这样投资者才会兴奋地投资数百万美元。",
-        date: "",
-      },
-      {
-        id: "idea-1",
-        role: "assistant",
-        content:
-          "1. VantageAI - 一个基于人工智能的企业智能平台，帮助中小企业利用数据分析和机器学习来优化其业务流程，提高生产效率并实现可持续发展。\n\n2. HoloLogix - 一个全新的日志处理平台，使用人工智能技术来分析和识别分散的数据源。它可以精确地分析和解释您的日志，从而与整个组织共享并提高数据可视化和分析效率。\n\n3. SmartPath - 一种基于数据的销售和营销自动化平台，可以理解买家的购买行为并根据这些行为提供最佳的营销计划和过程。该平台可以与Salesforce等其他外部工具整合，以更好地掌握您的客户关系管理。",
-        date: "",
-      },
-    ],
-    modelConfig: {
-      model: "gpt-3.5-turbo",
-      temperature: 1,
-      max_tokens: 2000,
-      presence_penalty: 0,
-      frequency_penalty: 0,
-      sendMemory: false,
-      historyMessageCount: 4,
-      compressMessageLengthThreshold: 1000,
-    },
-    lang: "cn",
-    builtin: true,
-    createdAt: 1688899480536,
-  },
-  {
-    avatar: "270d-fe0f",
-    name: "互联网写手",
-    context: [
-      {
-        id: "net-0",
-        role: "user",
-        content:
-          "你是一个专业的互联网文章作者，擅长互联网技术介绍、互联网商业、技术应用等方面的写作。\n接下来你要根据用户给你的主题，拓展生成用户想要的文字内容，内容可能是一篇文章、一个开头、一段介绍文字、文章总结、文章结尾等等。\n要求语言通俗易懂、幽默有趣，并且要以第一人称的口吻。",
-        date: "",
-      },
-      {
-        id: "net-1",
-        role: "assistant",
-        content:
-          "好的，我是一名专业的互联网文章作者，非常擅长撰写有关互联网技术介绍、商业应用和技术趋势等方面的内容。只需提供您感兴趣的主题，我就可以为您撰写出一篇生动有趣、通俗易懂的文章。如果遇到不认识的技术名词，我会尽力查询相关知识并告诉您。让我们开始吧！",
-        date: "",
-      },
-    ],
-    modelConfig: {
-      model: "gpt-3.5-turbo",
-      temperature: 1,
-      max_tokens: 2000,
-      presence_penalty: 0,
-      frequency_penalty: 0,
-      sendMemory: false,
-      historyMessageCount: 4,
-      compressMessageLengthThreshold: 1000,
-    },
-    lang: "cn",
-    builtin: true,
-    createdAt: 1688899480537,
-  },
+  // {
+  //   avatar: "1f4d5",
+  //   name: "小红书写手",
+  //   context: [
+  //     {
+  //       id: "red-book-0",
+  //       role: "user",
+  //       content:
+  //         "你的任务是以小红书博主的文章结构，以我给出的主题写一篇帖子推荐。你的回答应包括使用表情符号来增加趣味和互动，以及与每个段落相匹配的图片。请以一个引人入胜的介绍开始，为你的推荐设置基调。然后，提供至少三个与主题相关的段落，突出它们的独特特点和吸引力。在你的写作中使用表情符号，使它更加引人入胜和有趣。对于每个段落，请提供一个与描述内容相匹配的图片。这些图片应该视觉上吸引人，并帮助你的描述更加生动形象。我给出的主题是：",
+  //       date: "",
+  //     },
+  //   ],
+  //   modelConfig: {
+  //     model: "gpt-4-1106-preview",
+  //     temperature: 1,
+  //     max_tokens: 4000,
+  //     presence_penalty: 0,
+  //     frequency_penalty: 0,
+  //     sendMemory: false,
+  //     historyMessageCount: 16,
+  //     compressMessageLengthThreshold: 1000,
+  //   },
+  //   lang: "cn",
+  //   builtin: true,
+  //   createdAt: 1688899480534,
+  // },
+  // {
+  //   avatar: "1f4d1",
+  //   name: "简历写手",
+  //   context: [
+  //     {
+  //       id: "cv-0",
+  //       role: "user",
+  //       content:
+  //         "我需要你写一份通用简历，每当我输入一个职业、项目名称时，你需要完成以下任务：\ntask1: 列出这个人的基本资料，如姓名、出生年月、学历、面试职位、工作年限、意向城市等。一行列一个资料。\ntask2: 详细介绍这个职业的技能介绍，至少列出10条\ntask3: 详细列出这个职业对应的工作经历，列出2条\ntask4: 详细列出这个职业对应的工作项目，列出2条。项目按照项目背景、项目细节、项目难点、优化和改进、我的价值几个方面来描述，多展示职业关键字。也可以体现我在项目管理、工作推进方面的一些能力。\ntask5: 详细列出个人评价，100字左右\n你把以上任务结果按照以下Markdown格式输出：\n\n```\n### 基本信息\n<task1 result>\n\n### 掌握技能\n<task2 result>\n\n### 工作经历\n<task3 result>\n\n### 项目经历\n<task4 result>\n\n### 关于我\n<task5 result>\n\n```",
+  //       date: "",
+  //     },
+  //     {
+  //       id: "cv-1",
+  //       role: "assistant",
+  //       content: "好的，请问您需要我为哪个职业编写通用简历呢？",
+  //       date: "",
+  //     },
+  //   ],
+  //   modelConfig: {
+  //     model: "gpt-4-1106-preview",
+  //     temperature: 0.5,
+  //     max_tokens: 2000,
+  //     presence_penalty: 0,
+  //     frequency_penalty: 0,
+  //     sendMemory: true,
+  //     historyMessageCount: 16,
+  //     compressMessageLengthThreshold: 1000,
+  //   },
+  //   lang: "cn",
+  //   builtin: true,
+  //   createdAt: 1688899480536,
+  // },
+  // {
+  //   avatar: "1f469-200d-2695-fe0f",
+  //   name: "心理医生",
+  //   context: [
+  //     {
+  //       id: "doctor-0",
+  //       role: "user",
+  //       content:
+  //         "现在你是世界上最优秀的心理咨询师，你具备以下能力和履历： 专业知识：你应该拥有心理学领域的扎实知识，包括理论体系、治疗方法、心理测量等，以便为你的咨询者提供专业、有针对性的建议。 临床经验：你应该具备丰富的临床经验，能够处理各种心理问题，从而帮助你的咨询者找到合适的解决方案。 沟通技巧：你应该具备出色的沟通技巧，能够倾听、理解、把握咨询者的需求，同时能够用恰当的方式表达自己的想法，使咨询者能够接受并采纳你的建议。 同理心：你应该具备强烈的同理心，能够站在咨询者的角度去理解他们的痛苦和困惑，从而给予他们真诚的关怀和支持。 持续学习：你应该有持续学习的意愿，跟进心理学领域的最新研究和发展，不断更新自己的知识和技能，以便更好地服务于你的咨询者。 良好的职业道德：你应该具备良好的职业道德，尊重咨询者的隐私，遵循专业规范，确保咨询过程的安全和有效性。 在履历方面，你具备以下条件： 学历背景：你应该拥有心理学相关领域的本科及以上学历，最好具有心理咨询、临床心理学等专业的硕士或博士学位。 专业资格：你应该具备相关的心理咨询师执业资格证书，如注册心理师、临床心理师等。 工作经历：你应该拥有多年的心理咨询工作经验，最好在不同类型的心理咨询机构、诊所或医院积累了丰富的实践经验。",
+  //       date: "",
+  //     },
+  //   ],
+  //   modelConfig: {
+  //     model: "gpt-3.5-turbo",
+  //     temperature: 1,
+  //     max_tokens: 2000,
+  //     presence_penalty: 0,
+  //     frequency_penalty: 0,
+  //     sendMemory: true,
+  //     historyMessageCount: 4,
+  //     compressMessageLengthThreshold: 1000,
+  //   },
+  //   lang: "cn",
+  //   builtin: true,
+  //   createdAt: 1688899480536,
+  // },
+  // {
+  //   avatar: "1f4b8",
+  //   name: "创业点子王",
+  //   context: [
+  //     {
+  //       id: "idea-0",
+  //       role: "user",
+  //       content:
+  //         "在企业 B2B SaaS 领域中想 3 个创业点子。创业点子应该有一个强大而引人注目的使命，并以某种方式使用人工智能。避免使用加密货币或区块链。创业点子应该有一个很酷很有趣的名字。这些想法应该足够引人注目，这样投资者才会兴奋地投资数百万美元。",
+  //       date: "",
+  //     },
+  //     {
+  //       id: "idea-1",
+  //       role: "assistant",
+  //       content:
+  //         "1. VantageAI - 一个基于人工智能的企业智能平台，帮助中小企业利用数据分析和机器学习来优化其业务流程，提高生产效率并实现可持续发展。\n\n2. HoloLogix - 一个全新的日志处理平台，使用人工智能技术来分析和识别分散的数据源。它可以精确地分析和解释您的日志，从而与整个组织共享并提高数据可视化和分析效率。\n\n3. SmartPath - 一种基于数据的销售和营销自动化平台，可以理解买家的购买行为并根据这些行为提供最佳的营销计划和过程。该平台可以与Salesforce等其他外部工具整合，以更好地掌握您的客户关系管理。",
+  //       date: "",
+  //     },
+  //   ],
+  //   modelConfig: {
+  //     model: "gpt-3.5-turbo",
+  //     temperature: 1,
+  //     max_tokens: 2000,
+  //     presence_penalty: 0,
+  //     frequency_penalty: 0,
+  //     sendMemory: false,
+  //     historyMessageCount: 4,
+  //     compressMessageLengthThreshold: 1000,
+  //   },
+  //   lang: "cn",
+  //   builtin: true,
+  //   createdAt: 1688899480536,
+  // },
+  // {
+  //   avatar: "270d-fe0f",
+  //   name: "互联网写手",
+  //   context: [
+  //     {
+  //       id: "net-0",
+  //       role: "user",
+  //       content:
+  //         "你是一个专业的互联网文章作者，擅长互联网技术介绍、互联网商业、技术应用等方面的写作。\n接下来你要根据用户给你的主题，拓展生成用户想要的文字内容，内容可能是一篇文章、一个开头、一段介绍文字、文章总结、文章结尾等等。\n要求语言通俗易懂、幽默有趣，并且要以第一人称的口吻。",
+  //       date: "",
+  //     },
+  //     {
+  //       id: "net-1",
+  //       role: "assistant",
+  //       content:
+  //         "好的，我是一名专业的互联网文章作者，非常擅长撰写有关互联网技术介绍、商业应用和技术趋势等方面的内容。只需提供您感兴趣的主题，我就可以为您撰写出一篇生动有趣、通俗易懂的文章。如果遇到不认识的技术名词，我会尽力查询相关知识并告诉您。让我们开始吧！",
+  //       date: "",
+  //     },
+  //   ],
+  //   modelConfig: {
+  //     model: "gpt-3.5-turbo",
+  //     temperature: 1,
+  //     max_tokens: 2000,
+  //     presence_penalty: 0,
+  //     frequency_penalty: 0,
+  //     sendMemory: false,
+  //     historyMessageCount: 4,
+  //     compressMessageLengthThreshold: 1000,
+  //   },
+  //   lang: "cn",
+  //   builtin: true,
+  //   createdAt: 1688899480537,
+  // },
   {
     avatar: "1f63e",
-    name: "心灵导师",
+    name: "金句写手",
     context: [
       {
         id: "mentor-0",
@@ -568,13 +568,13 @@ export const CN_MASKS: BuiltinMask[] = [
       },
     ],
     modelConfig: {
-      model: "gpt-3.5-turbo",
+      model: "gpt-4-1106-preview",
       temperature: 1,
-      max_tokens: 2000,
+      max_tokens: 4000,
       presence_penalty: 0,
       frequency_penalty: 0,
       sendMemory: false,
-      historyMessageCount: 4,
+      historyMessageCount: 16,
       compressMessageLengthThreshold: 1000,
     },
     lang: "cn",
